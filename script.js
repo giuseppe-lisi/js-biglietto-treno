8e86902802567559c1c50847552217799ceff371
Giuseppe Lisi <giuseppe.lisi.main@gmail.com>
2025-10-25 19:36:29 +0200

    resa UI funzionale

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
-function finalPrice(kilometers, age) {
+//fetch dati utente quando si clicca il bottone calcola prezzo
+function getUserData() {
+    let kmUser = parseInt(document.getElementById("km").value);
+    let ageUser = parseInt(document.getElementById("age").value);
+
+    //passo i dati alla funzione che calcola il prezzo
+    calculateFinalPrice(kmUser, ageUser);
+}
+
+function calculateFinalPrice(kilometers, age) {
     //calcolo prezzo con tariffa di base
     let price = kilometers * 0.21;
 
@@ -8,12 +17,7 @@ function finalPrice(kilometers, age) {
     } else if (age >= 65) {
         price -= (price / 100) * 40;
     }
-    return price.toFixed(2); 
+
+    //restituisco il prezzo del biglietto
+    document.getElementById("ticketPrice").innerHTML = price.toFixed(2) + "€";
 }
-
-//richiesta dati utente
-const kmUser = parseInt(prompt("Quanti Km dovrai percorrere?"));
-const ageUser = parseInt(prompt("Quanti anni hai?"));
-
-//fornisco il prezzo finale all'utente
-document.getElementById("prezzo").innerHTML = finalPrice(kmUser, ageUser) + "€";
